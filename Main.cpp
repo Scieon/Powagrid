e50f48669bc4ad213439ccb2dee93e334e9785b6
Scieon <anhkhoi.vunguyen@gmail.com>
2016-02-15 20:34:46 -0500

    Update Main

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include "Player.h";
+#include "Powerplant.h";
+#include "ResourceManager.h"
+
 using namespace std;
 
 int main() {
 
 	Player p1;
+	Powerplant* pp1 ;
 	Player p2("blue");
 	Player *p3 = new Player("red");
 	p2.showInfo();
 
+	Oil o1;
 
-
-
+	int cost = o1.getCost();
+	o1.setCost(200);
+	cout << "Cost is " << cost << endl;
+	cost = o1.getCost();
+	cout << "Cost is " << cost << endl;
+	
 
 	cin.get();
 	return 0;
