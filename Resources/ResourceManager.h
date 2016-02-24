4516c2f03d6fc468ea7ec816c92d86e284d58427
Scieon <anhkhoi.vunguyen@gmail.com>
2016-02-23 23:35:26 -0500

    Tentative resource manager TOFIX

--- Resources/ResourceManager.h
+++ Resources/ResourceManager.h
@@ -1,24 +1,13 @@
+
+
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+
 #include "Coal.h"
 #include "Oil.h"
-#include "Uranium.h";
+#include "Uranium.h"
 #include "Garbage.h"
-
-#ifndef RESOURCE_MANAGER_H
-#define RESOURCE_MANAGER_H
-
-class ResourceManager {
-
-
-
-public:
-
-
-private:
-	Coal coal;
-	Garbage garbage;
-	Oil oil;
-	Uranium uranium;
-};
 
 
 #endif
